63d414b61e5639f911aa10f44c08dffccc5f98b3
Boriwan <boris.bocek@educanet.com>
2022-10-04 01:18:01 +0200

    progress

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -71,7 +71,6 @@ export default function Dashboard({ code }) {
                             key={track.uri}
                             chooseTrack={chooseTrack}
                         />
-
                     ))}
                 </div>
             </div>
--- client/src/components/TrackSearchResult.js
+++ client/src/components/TrackSearchResult.js
@@ -5,16 +5,18 @@ export default function TrackSearchResult({ track, chooseTrack }) {
         chooseTrack(track);
     }
     return (
-        <div className="song">
-            <div className='tracks' onClick={handlePlay}>
-                <img src={track.albumUrl} style={{ height: "50px", width: "50px" }} />
-                <div style={{ marginLeft: "3rem" }} >
-                    <div>{track.title}</div>
-                    <div style={{ opacity: "0.5" }}>{track.artist}</div>
+        <div>
+            <div className="song">
+                <div className='tracks' onClick={handlePlay}>
+                    <img src={track.albumUrl} style={{ height: "50px", width: "50px" }} />
+                    <div style={{ marginLeft: "3rem" }} >
+                        <div>{track.title}</div>
+                        <div style={{ opacity: "0.5" }}>{track.artist}</div>
+                    </div>
                 </div>
             </div>
-            <Divider variant="middle" style={{ margin: "4rem" }} />
-        </div>
+            <Divider variant="middle" style={{ margin: "1rem" }} />
+        </div >
 
     )
 }
